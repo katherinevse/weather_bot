54acb3d58a23976815216aa0ccde749d516fb2a4
Katherine <katherinevse@mail.ru>
2023-08-17 15:18:23 +0300

    day_indices

--- weather_bot.py
+++ weather_bot.py
@@ -1,10 +1,11 @@
 import time
 import logging
-from aiogram import Bot, Dispatcher, executor, types
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
+
 
 TOKEN = "6013011791:AAGSYCm8Qeo94l9pqGApyS2lkTRRVcbrzUM"
 MSG = "Я могу рассказать вам про погоду только в Москве!"
@@ -18,26 +19,18 @@ def fetch_weather_data():
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        weather_all = soup.find_all('span', {'class': 'text text_block text_bold_medium margin_bottom_10'})
-        condition_all = soup.find_all('span', {'class': 'text text_block text_light_normal text_fixed'})
-        feels_like = soup.find_all('span', {'class': 'text text_block text_light_normal text_fixed color_gray'})
+        weather_blocks = soup.find_all('div', {'class': 'day__temperature'})
+        condition_blocks = soup.find_all('div', {'class': 'day__description'})
+        feels_like_blocks = soup.find_all('div', {'class': 'day__feels-like'})
 
-        return weather_all, condition_all, feels_like
+        return weather_blocks, condition_blocks, feels_like_blocks
     except requests.exceptions.RequestException as e:
         logging.error(f"Error while fetching weather data: {e}")
         return None, None, None
 
 # Функция для форматирования сообщения о погоде
-def get_message(weather_all, condition_all, feels_like):
-    if not weather_all or not condition_all or not feels_like:
-        return 'Сайтик лёг, пусть отдохнет немножечко)'
-
-    result = ''
-    times = ('ночью', 'утром', 'днем', 'вечером')
-    for i, time_of_day in enumerate(times):
-        result += f'Погода {time_of_day}: {weather_all[i].text}, {condition_all[i].text}, {feels_like[i].text}\n'
-
-    return result
+def format_weather_message(time_of_day, weather, condition, feels_like):
+    return f'Погода {time_of_day}: {weather}, {condition}, {feels_like}'
 
 # Настройка бота и диспетчера
 bot = Bot(token=TOKEN)
@@ -48,13 +41,10 @@ dp = Dispatcher(bot=bot)
 async def start_handler(message: types.Message):
     user_id = message.from_user.id
     user_name = message.from_user.first_name
-    user_full_name = message.from_user.full_name
 
     # Работа с кнопками:
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add("Погода на сегодня", "Погода на завтра", "Погода на послезавтра")
-
-    logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
 
     current_time = datetime.now()
     if current_time.hour >= 17:
@@ -68,19 +58,29 @@ async def start_handler(message: types.Message):
     await message.answer("В какой день вы хотите узнать погоду в Москве?", reply_markup=keyboard)
 
 @dp.message_handler(Text(equals="Погода на сегодня"))
-async def today_weather(message: types.Message):
-    weather_all, condition_all, feels_like = fetch_weather_data()
-    await message.answer(get_message(weather_all[:4], condition_all[:4], feels_like[:4]))
-
 @dp.message_handler(Text(equals='Погода на завтра'))
-async def tomorrow_weather(message: types.Message):
-    weather_all, condition_all, feels_like = fetch_weather_data()
-    await message.answer(get_message(weather_all[4:8], condition_all[4:8], feels_like[4:8]))
-
 @dp.message_handler(Text(equals='Погода на послезавтра'))
-async def day_after_tomorrow_weather(message: types.Message):
-    weather_all, condition_all, feels_like = fetch_weather_data()
-    await message.answer(get_message(weather_all[8:12], condition_all[8:12], feels_like[8:12]))
+async def weather_by_day(message: types.Message):
+    day = message.text.replace("Погода на ", "")
+    weather_blocks, condition_blocks, feels_like_blocks = fetch_weather_data()
+
+    if not weather_blocks or not condition_blocks or not feels_like_blocks:
+        await message.answer("Произошла ошибка. Пожалуйста, попробуйте позже.")
+        return
+
+    day_indices = {'сегодня': 0, 'завтра': 1, 'послезавтра': 2}
+    index = day_indices.get(day)
+
+    if index is not None:
+        start = index * 4
+        end = start + 4
+        weather_data = zip(weather_blocks[start:end], condition_blocks[start:end], feels_like_blocks[start:end])
+        messages = [format_weather_message(time_of_day, weather.text, condition.text, feels_like.text)
+                    for time_of_day, weather, condition, feels_like in weather_data]
+
+        await message.answer("\n".join(messages))
+    else:
+        await message.answer("Извините, не могу предоставить погоду на выбранный день.")
 
 if __name__ == '__main__':
     executor.start_polling(dp)
